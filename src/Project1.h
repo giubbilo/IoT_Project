94dc9a150514c9b92cc4cf640124a510f99e0331
Davide Giannubilo <92432998+giubbilo@users.noreply.github.com>
2023-06-13 18:37:33 +0200

    Upload files

---
+++ src/Project1.h
@@ -0,0 +1,38 @@
+#ifndef PROJECT1_H
+#define PROJECT1_H
+
+typedef nx_struct Msg
+{
+  	nx_uint8_t type;
+  	/*
+	* type = 0 -> CONN
+	* type = 1 -> CONNACK
+	* type = 2 -> SUB
+	* type = 3 -> SUBACK
+	* type = 4 -> PUB
+	*/
+	nx_uint8_t sender;
+	nx_uint8_t dest;
+	nx_uint8_t data; // Field containing the value to send to Node-RED
+	nx_uint8_t topic;
+	/*
+	* topic = 0 -> TEMPERATURE
+	* topic = 1 -> HUMIDITY
+	* topic = 2 -> LUMINOSITY
+	*/
+}msg_t;
+
+uint16_t indexConnReceived[8] = {0}; // Containing which node have sent and delivered a CONN message
+uint16_t indexConnAckReceived[8] = {0}; // Containing which node have received a CONNACK message
+uint16_t indexSubSended[8] = {0}; // Containing which node have sent a SUB message
+uint16_t indexSubReceived[8] = {0}; // Containing which node have received a SUB message
+uint16_t indexSubAckReceived[8] = {0}; // Containing which node have received a SUBACK message
+uint16_t indexSubbedTopic[8] = {3, 3, 3, 3, 3, 3, 3, 3}; // Containing the topic of each node
+
+enum
+{
+	AM_RADIO_COUNT_MSG = 6,
+};
+
+#endif
+
